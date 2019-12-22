ec12ddffbae96b4974ac85acc625ad8c4ce5fed4
yamaday <e155417k@st.u-gakugei.ac.jp>
2019-12-22 11:37:09 +0900

    add: lexer_test.go
    字句解析器のテストコードを追加

---
+++ lexer/lexer_test.go
@@ -0,0 +1,40 @@
+package lexer
+
+import (
+	"testing"
+
+	"github.com/wf-yamaday/monkey/token"
+)
+
+func TestNextToken(t *testing.T) {
+	input := `=+(){},;`
+
+	tests := []struct {
+		expextedType    token.TokenType
+		expextedLiteral string
+	}{
+		{token.ASSIGN, "="},
+		{token.PLUS, "+"},
+		{token.LPAREN, "("},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RBRACE, "}"},
+		{token.COMMA, ","},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+
+	l := New(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+
+		if tok.Type != tt.expextedType {
+			t.Fatalf("test[%d] - tokentype wrong. expected=%q, got=%q", i, tt.expextedType, tok.Type)
+		}
+
+		if tok.Literak != tt.expextedLiteral {
+			t.Fatalf("test[%d - literal wrong. expected=%q, got=%q", i, tt.expextedLiteral, tok.Type)
+		}
+	}
+}

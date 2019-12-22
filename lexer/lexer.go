ce0561d7ede2f9d31ae4b899e6418dff00e7b784
yamaday <e155417k@st.u-gakugei.ac.jp>
2019-12-22 11:42:29 +0900

    add lexer.go

---
+++ lexer/lexer.go
@@ -0,0 +1,24 @@
+package lexer
+
+type Lexer struct {
+	input        string
+	position     int  //入力における現在の文字
+	readPosition int  //これから読み込む位置
+	ch           byte //現在検査中の文字
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition++
+}
